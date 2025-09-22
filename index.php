7ae188411172720b327cc1a5e6840cdedfcdb741
merve-goncu <merve.gnc.946@gmail.com>
2025-09-22 14:45:28 +0300

    firstly types

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$name ="Merve";
+$age ="23";
+echo "My name is $name an I am $age years old.";
+
+
+$a =10;
+$b =20;
+
+$addition = $a + $b;
+$substraction = $a - $b;
+$multiplication = $a * $b;
+$division = $b / $a;
+echo "<br/>Addition: $addition 
+<br/>Substraction: $substraction 
+<br/>Multiplication: $multiplication 
+<br/>Division: $division";
+
+
+
+
+$age = 18;
+
+if ($age >= 18) {
+    echo "<br/>Reşitsiniz.";
+} else {
+    echo "<br/>Reşit değilsiniz.";
+}
+
+
+ 
+
+
+?>
