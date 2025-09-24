c90054248450dbfcd2fe1954eb2c09c8fb004ef1
merve-goncu <merve.gnc.946@gmail.com>
2025-09-24 11:41:51 +0300

    Basic operates

--- index.php
+++ index.php
@@ -17,18 +17,73 @@ echo "<br/>Addition: $addition
 <br/>Division: $division";
 
 
-
+#if else
 
 $age = 18;
 
 if ($age >= 18) {
-    echo "<br/>Reşitsiniz.";
+    echo "You are an adult.";
 } else {
-    echo "<br/>Reşit değilsiniz.";
+    echo "You are not an adult.";
 }
 
-
+#while loop
  
+$i = 1;
 
+while ($i <= 5) {
+    echo "<br>Number: $i";
+    $i++;
+    echo"while loop";
+}
+
+#for loop
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "<br>Number: $i";
+    echo"for loop";
+}
+
+#foreach loop
+
+$colors = array("Red", "Green", "Blue");
+
+foreach ($colors as $color) {
+    echo "<br>Colour: $color ";
+}
+
+function greet($name) {
+    echo "<br>Hello, $name! <br>";
+}
+
+greet("Merve");
+greet("Hugh");
+
+
+
+// Define a function called "average" that takes one parameter: $numbers (an array)
+function average($numbers) {
+    // Start with a sum of 0
+    $sum = 0;
+
+    // Count how many items are inside the array
+    $count = count($numbers);
+
+    // Loop through each number in the array
+    foreach ($numbers as $num) {
+        // Add the current number to the running total
+        $sum += $num;
+    }
+
+    // Divide the total sum by the number of items to get the average
+    return $sum / $count;
+}
+
+// Create an array of grades
+$grades = [80, 90, 75, 100];
+
+// Call the average() function with the $grades array
+// and print the result with a label
+echo "<br><br>Average: " . average($grades);
 
 ?>
