a8614db1581a0853131d165ab8e07564155d23de
merve-goncu <merve.gnc.946@gmail.com>
2025-09-29 14:01:46 +0300

    Some Server informations

--- lessonOne/index.php
+++ lessonOne/index.php
@@ -9,10 +9,15 @@
 
 <body>
     <?php
-    $name = "John";
-    $valueToBeAssaigned = $name;
+    echo $_SERVER["DOCUMENT_ROOT"];
+    echo "<br>";
+    echo $_SERVER["PHP_SELF"];
+    echo "<br>";
+    echo $_SERVER["SERVER_NAME"];
+    echo "<br>";
+    echo $_SERVER["REQUEST_METHOD"];
     ?>
-    <p>Hello I'am <?php echo $valueToBeAssaigned; ?>, and Iam Mastering PHP</p>
+   
 </body>
 
 </html>
