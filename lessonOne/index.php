98aa24373aaf9b22b1a3d0b568827f770a1ed26b
merve-goncu <merve.gnc.946@gmail.com>
2025-09-29 11:51:07 +0300

    Variables and default versions of them

--- lessonOne/index.php
+++ lessonOne/index.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
-<html   lang="en">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> 
-    </head>
+    <title>Document</title>
+</head>
+
 <body>
-  <p>Lesson One</p>
-  <?php
-  echo "Hello World";
-  ?>
+    <?php
+    /*A variable is like a box with a label for what it holds, and variables have dollar signs in front of their names*/
+    //Scalar data types: string, integer, float, boolean
+    $string = "Hello World";
+    $integer = 42;
+    $float = 3.14;
+    $boolean = true;
+
+    //array data types: array, object
+    $array = array("John", "Jane", "Doe");
+    $array = ["John", "Jane", "Doe"];
+    //default
+    $string = "";
+    $integer = 0;
+    $float = 0;
+    $boolean = false;
+    $array = [];
+    $object = null;
+    ?>
 </body>
+
 </html>
-
-
