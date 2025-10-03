2912f43368ba73c938481c46e979f10b94066fc0
merve-goncu <merve.gnc.946@gmail.com>
2025-10-03 11:01:42 +0300

    The Basics of PHP Form Handling

---
+++ lessonThree/index.php
@@ -0,0 +1,25 @@
+<html>
+
+<body>
+    <main>
+        <form action="includes/formhandler.php" method="get">
+            <label for="firstname">Firstname?</label>
+            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+
+            <label for="lastname">Lastname?</label>
+            <input id="lastname" type="text" name="lastname" placeholder="Lastname...">
+
+            <label for="favouritepet">Favourite Pet?</label>
+            <select id="favouritepet" name="favouritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="bird">Bird</option>
+            </select>
+
+            <button type="submit">Submit</button>
+        </form>
+    </main>
+</body>
+
+</html>
